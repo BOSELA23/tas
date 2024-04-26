eed1e0f78425fa3414b757110301f84cdebee1a4
BOSELA23 <161165285+BOSELA23@users.noreply.github.com>
2024-04-26 02:59:00 -0700

    Update web.js

--- web.js
+++ web.js
@@ -1,29 +1,61 @@
-let scrollUpButton = document.querySelector(".move-up-button");
+window.addEventListener('load', function() {
+  let preload = document.querySelector('.preload');
+  if (preload) {
+      preload.style.display = 'none';
+  }
+});
 
-  window.addEventListener("scroll", function() {
-    if (window.scrollY > 2) {
-      scrollUpButton.style.display = "block";
-    } else {
-      scrollUpButton.style.display = "none";
-    }
-  });
 
-  scrollUpButton.addEventListener("click", function() {
+var scrollUpBtn = document.getElementById("scrollUpBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollUpBtn.style.display = "block";
+  } else {
+    scrollUpBtn.style.display = "none";
+  }
+};
+  scrollUpBtn.onclick = function () {
     window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  });
-
-  let button = document.querySelector(".swiperight");
-
-  button.addEventListener("click", () => {
-    window.scrollBy({
-      left: 100,
       top: 0,
       behavior: "smooth",
     });
-  });
+  };
 
-  
-   
+
+let currentIndex = 0;
+
+function moveSlide(direction) {
+  const sliders = document.querySelectorAll(".slider");
+  const totalSlides = sliders.length;
+
+  if (direction === "right") {
+    currentIndex = (currentIndex + 1) % totalSlides;
+  } else if (direction === "left") {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+  }
+
+  const offset = currentIndex * -200;
+  sliders.forEach((slider) => {
+    slider.style.transform = `translateX(${offset}px)`;
+  });
+}
+
+
+
+
+
+function filterSelection(category) {
+  const elements = document.querySelectorAll('.selection img');
+  if (category === 'all') {
+      elements.forEach(el => el.style.display = '');
+  } else {
+      elements.forEach(el => {
+          if (el.classList.contains(category)) {
+              el.style.display = '';
+          } else {
+              el.style.display = 'none';
+          }
+      });
+  }
+}
